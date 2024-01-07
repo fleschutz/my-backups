58db53b380bf14e777be210f08170916a7dce83f
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-01-07 16:01:17 +0100

    Update '02 Backup my Screenshots.bat'

--- 02 Backup my Screenshots.bat
+++ 02 Backup my Screenshots.bat
@@ -1,13 +1,15 @@
 @echo off
 ::
-:: Performs a backup from local Dropbox to drive M:
+:: Performs a backup from local screenshots to drive M:
 :: ================================================
 
-set SRC=C:\Users\Markus\Dropbox\
-set DST=M:\Backup\data\Dropbox\
+set MY_BACKUP=%~dp0
+set SRC=%HOMEPATH%\Pictures\Screenshots\
+set DST=%MY_BACKUP%\Screenshots\
 set OPTIONS=/MIR /FFT /MT:8
 
 title Performing backup of %SRC% to %DST% ...
+if not exist "%DST%" mkdir "%DST%"
 robocopy  %SRC%	%DST% %OPTIONS%
 
 echo OK - backup of %SRC% to %DST% completed.
