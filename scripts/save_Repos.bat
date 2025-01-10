917ee398996960181e3f48f86e0187edb19cfcdd
Markus Fleschutz <markus.fleschutz@gmail.com>
2025-01-10 15:41:26 +0100

    Added save_Repos.bat

---
+++ scripts/save_Repos.bat
@@ -0,0 +1,55 @@
+@echo off
+title Backup of Git Repositories
+
+set "SOURCE=C:\Repos"
+set "TARGET=%~dp0..\my_data\Repos\"
+set OPTIONS=/mir /xa:o /r:3 /w:10 /fft /njh /unicode
+set LOGFILE=..\Logbook.csv
+
+cls
+echo.
+echo         ____________________________________
+echo.
+echo               Backup of Git Repositories
+echo         ____________________________________
+echo.
+echo.
+echo   This script will back up your Repos folder...
+echo.
+echo.
+echo.
+echo      FROM : %SOURCE% 
+echo.
+echo        TO : %TARGET% 
+echo.
+echo   OPTIONS : robocopy %OPTIONS%
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo Press [Return] to continue or abort with [Ctrl] [C]...
+pause
+
+echo %DATE%; %TIME%; %COMPUTERNAME%; %SOURCE%; Backup started; >>%LOGFILE%
+if not exist "%TARGET%" mkdir "%TARGET%"
+robocopy %SOURCE% %TARGET% %OPTIONS%
+echo %DATE%; %TIME%; %COMPUTERNAME%; %SOURCE%; Backup finished; >>%LOGFILE%
+
+echo ________________________________________________________________________________________
+echo.
+echo         #
+echo        #
+echo   #   #      Repos folder backed up.
+echo    # #
+echo     #            
+echo.
+echo NOTE: When finished disconnect the drive and store it in a separate and secure location!
+echo ________________________________________________________________________________________
+echo.
+pause
+exit 0
