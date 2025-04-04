a236e4cb11d8cd65a7786d61752ec08b818bba56
Markus Fleschutz <markus.fleschutz@gmail.com>
2025-04-04 14:52:55 +0200

    Updated save_Pictures.bat

--- scripts/save_Pictures.bat
+++ scripts/save_Pictures.bat
@@ -32,18 +32,19 @@ echo.
 echo.
 echo.
 echo.
-echo Press [Return] to continue or abort with [Ctrl] [C]...
-pause
+echo Backup about to start or abort with [Ctrl] [C]...
+timeout 5
+
 
 echo %DATE%; %TIME%; %COMPUTERNAME%; %SOURCE%; Backup started; >>%LOGFILE%
 if not exist "%TARGET%" mkdir "%TARGET%"
 robocopy %SOURCE% %TARGET% %OPTIONS%
 echo %DATE%; %TIME%; %COMPUTERNAME%; %SOURCE%; Backup finished; >>%LOGFILE%
 
-title Pictures backed up!
 chcp 65001 >NUL
-echo ✅ Pictures folder backed up.
+title ✅ Pictures backed up
+echo ✅ Backup of your pictures folder succeeded.
 echo.
-echo NOTE: when finished disconnect the drive and store it in a separate(!) and secure(!) location.
+echo NOTE: when finished eject the drive properly and store it in a separate(!) and secure(!) location.
 pause
 exit 0
